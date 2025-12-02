dbf2e435ddb5725aeef324122a246876e5424bfb
Luiz Antonio de Freitas <lafreitas2@yahoo.com.br>
2025-12-02 20:10:10 -0300

    Segundo commit: settings, admin, forms e sqlite3

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig', # added users app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'age', 'is_staff'] # Display age in admin list view
+
+admin.site.register(CustomUser, CustomUserAdmin)
