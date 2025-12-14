056aa5cd765ef1a04ecdd6ab08d25b531ef31f0b
Luiz Antonio de Freitas <lafreitas2@yahoo.com.br>
2025-12-13 21:34:36 -0300

    Comentários. Atualizando admin.py, models.py e SQLite

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,12 @@
+# articles/admin.py
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment # import Article model
 
-admin.site.register(Article)
+class CommentInLine(admin.TabularInline): # inline admin for comments
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInLine,] # include comments inline in article admin
+
+admin.site.register(Article, ArticleAdmin) # register article model with custom admin
+admin.site.register(Comment) # register comment model to admin site
--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,8 @@
-from django.conf import settings
+# articles/models.py
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
@@ -12,8 +13,28 @@ class Article(models.Model):
         on_delete=models.CASCADE,
     )
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-def get_absolute_url(self):
-    return reverse('article-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('article-detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article,
+        related_name='comments',
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    comment = models.TextField()  # texto do comentário
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment[:50]
+
+    def get_absolute_url(self):
+        return reverse('article-detail', args=[str(self.article.id)])
